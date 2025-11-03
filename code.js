4a6a12d7605c6a2924130ecbcd55c7fcdcc41b9d
Vladislav Sivikhin <sivlad004@yandex.ru>
2025-11-03 22:08:40 +0300

    Upd tasks and small fixes

--- code.js
+++ code.js
@@ -2,8 +2,9 @@ const add_button = document.querySelector(".add_button")
 const tasks_list = document.querySelector(".tasks_list")
 
 // Что-то в духе Enum`а
-const Stage = { NotDone: "not done", InProgress: "in progress", Done: "done" }
+const Stage = { NotDone: "notdone", InProgress: "inprogress", Done: "done" }
 const Category = { Home: "home", Study: "study", Work: "work", Other: "other" }
+const Priority = { Low: "low", Medium: "medium", High: "high" }
 
 
 
@@ -31,10 +32,19 @@ const show_all_tasks = () => {
 const show_task = (task, index) => {
     return `
     <div class="task">
-        <p> ${task.name} - ${task.stage} - ${task.category}</p>
-        <button class="delete_button" onclick="del_task(${index})">del</button>
-        <button class="update_button" onclick="upd_task(${index})">upd</button>
+        <div class="task_left">
+            <div class="category_div color_${task.category}"></div>
+            <p class="task_name">${task.name}</p>
+            <div class="del_upd_buttons">
+                <button class="delete_button" onclick="del_task(${index})">del</button>
+                <button class="update_button" onclick="edit_task(${index})">edit</button>
+            </div>
+        </div>
+        <div class="task_right stage_${task.stage}">
+            <button class="next_button" onclick="next_step(${index})">></button>
+        </div>
     </div>
+    <div class="edit_window${index} edit_win"></div>
     `
 }
 
@@ -49,11 +59,44 @@ const set_task_value = (index, new_value) => {
         {
             "name": new_value,
             "stage": tasks[index].stage,
-            "categories": tasks[index].categories
+            "category": tasks[index].category
         },
         ...tasks.slice(index + 1)
     ]
     tasks = new_tasks
     // close form?
     show_all_tasks()
+}
+
+const set_task_stage = (index, new_stage) => {
+    let new_tasks = [
+        ...tasks.slice(0, index),
+        {
+            "name": tasks[index].name,
+            "stage": new_stage,
+            "category": tasks[index].category
+        },
+        ...tasks.slice(index + 1)
+    ]
+    tasks = new_tasks
+    show_all_tasks()
+}
+
+const next_step = (index) => {
+    switch (tasks[index].stage) {
+        case Stage.NotDone:
+            set_task_stage(index, Stage.InProgress)
+            break;
+        case Stage.InProgress:
+            set_task_stage(index, Stage.Done)
+            break;
+    }
+}
+
+const edit_task = (index) => {
+    const edit_window = document.querySelector(`.edit_window${index}`)
+    edit_window.classList.add("visible");
+    edit_window.innerHTML = `
+    <p>Test edit</p>
+    `;
 }
